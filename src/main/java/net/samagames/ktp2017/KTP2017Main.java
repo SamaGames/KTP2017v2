ae047fc16d08e61037623887f5317dfa561483d5
Vialonyx <pro.vialonyx@gmail.com>
2017-04-01 08:19:22 +0200

    feat(global): setting-up eternal day

--- src/main/java/net/samagames/ktp2017/KTP2017Main.java
+++ src/main/java/net/samagames/ktp2017/KTP2017Main.java
@@ -3,6 +3,8 @@ package net.samagames.ktp2017;
 import net.samagames.api.SamaGamesAPI;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import static org.bukkit.Bukkit.getWorlds;
+
 public class KTP2017Main extends JavaPlugin {
 
     /**
@@ -19,6 +21,10 @@ public class KTP2017Main extends JavaPlugin {
         this.game = new KTP2017("code", "KTP2017", "description", KTP2017Player.class);
         SamaGamesAPI.get().getGameManager().setFreeMode(true);
         SamaGamesAPI.get().getGameManager().registerGame(this.game);
+
+        // Updating gamerules values.
+        getWorlds().get(0).setGameRuleValue("doDaylightCycle", "false");
+        getWorlds().get(0).setTime(6000);
 
     }
 
