fa91bde139224f36dd0c51dfbef35068949779e5
Vialonyx <pro.vialonyx@gmail.com>
2017-04-08 09:20:34 +0200

    feat(internal_gamephase): Introduced GamePhases

--- src/main/java/net/samagames/ktp2017/KTP2017.java
+++ src/main/java/net/samagames/ktp2017/KTP2017.java
@@ -9,8 +9,24 @@ public class KTP2017 extends Game<KTP2017Player> {
      * @author Vialonyx
      */
 
+    private GamePhase current;
+
     public KTP2017(String gameCodeName, String gameName, String gameDescription, Class<KTP2017Player> gamePlayer) {
         super(gameCodeName, gameName, gameDescription, gamePlayer);
+
+        // Setting current phase to WAIT
+        this.current = GamePhase.WAIT;
+
+    }
+
+    public static enum GamePhase {
+
+        WAIT, AREA_STARTED, GAME_PHASE1, GAME_PHASE2, GAME_DONE;
+
+    }
+
+    public GamePhase getCurrentGamePhase(){
+        return this.current;
     }
 
 }
