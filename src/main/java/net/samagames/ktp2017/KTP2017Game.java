e40812907e215984a0cbf1dbc483c18ba6900a25
Vialonyx <pro.vialonyx@gmail.com>
2017-04-21 02:15:12 +0200

    feat(area): introduced currentPlayedArea and nextPlayableArea. Also teleporting player to the currentPlayerArea on join

--- src/main/java/net/samagames/ktp2017/KTP2017Game.java
+++ src/main/java/net/samagames/ktp2017/KTP2017Game.java
@@ -1,6 +1,8 @@
 package net.samagames.ktp2017;
 
 import net.samagames.api.games.Game;
+import org.bukkit.entity.Player;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -14,6 +16,8 @@ public class KTP2017Game extends Game<KTPPlayer> {
 
     private GamePhase current;
     private List<KTPArea> avaibleAreas;
+    private KTPArea currentlyPlayedArea;
+    private KTPArea nextPlayableArea;
 
     public static KTPArea area1 = new KTPArea(1);
     public static KTPArea area2 = new KTPArea(2);
@@ -25,14 +29,27 @@ public class KTP2017Game extends Game<KTPPlayer> {
         this.avaibleAreas = new ArrayList<>();
 
         // Registering areas
-        this.avaibleAreas.add(area1);
-        this.avaibleAreas.add(area2);
+        this.avaibleAreas.add(KTP2017Game.area1);
+        this.avaibleAreas.add(KTP2017Game.area2);
 
         // Setting current phase to WAIT
         this.current = GamePhase.WAIT;
 
+        // Set the current playable Area
+        this.currentlyPlayedArea = KTP2017Game.area1;
+
+        // Set the next playable Area (Generated automatically and randomly in the future!)
+        this.nextPlayableArea = KTP2017Game.area2;
+
         // Debugging game variables during developement phase
         logDebug();
+
+    }
+
+    @Override
+    public void handleLogin(Player player){
+
+        player.teleport(this.getCurrentlyPlayedArea().getAreaLocation());
 
     }
 
@@ -52,5 +69,11 @@ public class KTP2017Game extends Game<KTPPlayer> {
     public GamePhase getCurrentGamePhase(){
         return this.current;
     }
+
+    public KTPArea getCurrentlyPlayedArea(){
+        return this.currentlyPlayedArea;
+    }
+
+    public KTPArea getNextPlayableArea(){ return this.nextPlayableArea; }
 
 }
